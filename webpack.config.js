3ab4952cde5fb4c811f3dc5e58f939ca3bcd8591
Bruce Rabine <bruce.rabine@ties.k12.mn.us>
2017-10-03 00:06:36 -0500

    Got basic webpack build working

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,17 @@ module.exports = {
   entry: './src/index.js',
   output: { path: __dirname, filename: 'bundle.js' },
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
 };
